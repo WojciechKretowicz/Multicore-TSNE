663898f66ae64fd4c26f99769ed37b60752b49ae
WojciechKretowicz <wojtekkretowicz@gmail.com>
2019-04-15 19:09:53 +0200

    Minor corrections:

--- TraceTSNE/__init__.py
+++ TraceTSNE/__init__.py
@@ -5,17 +5,17 @@ class TraceTSNE:
                  frame_rate=30,
                  resolution="1920x1080",
                  output_file="trace_tsne.mp4"):
-	"""
-	Class is used to make a video of iteration of gradient descent.
+        """
+        Class is used to make a video of iteration of gradient descent.
 
-	size - tuple of length 2, describes the size of the each frame used in the video
-	ffmpeg_command - if you want to directly specify how the video is made,
-		it is a command line of ffmpeg, if you specify this, then the frame_rate
-		and resolution is ignored
-	frame_rate - frame rate of the video
-	resolution - resolution of the video
-	output_file - name of the output file
-	"""
+        size - tuple of length 2, describes the size of the each frame used in the video
+        ffmpeg_command - if you want to directly specify how the video is made,
+            it is a command line of ffmpeg, if you specify this, then the frame_rate
+            and resolution is ignored
+        frame_rate - frame rate of the video
+        resolution - resolution of the video
+        output_file - name of the output file
+        """
 
         self.size = size
         self.ffmpeg_command = ffmpeg_command
@@ -31,25 +31,26 @@ class TraceTSNE:
 
 
     def transform(self, TSNE, y=None):
-	"""
-	This method makes a video from the given MulticoreTSNE object.
+        """
+        This method makes a video from the given MulticoreTSNE object.
 
-	TSNE - MulticoreTSNE object after fit method
-	y - optional, the length must be equal to the number of points,
-		used to colorize the points
-	"""
+        TSNE - MulticoreTSNE object after fit method
+        y - optional, the length must be equal to the number of points,
+            used to colorize the points
+        """
 
         import seaborn as sns
         import os
 
         import matplotlib.pyplot as plt
         plt.rcParams['figure.figsize'] = self.size
-	
-	# making charts
+
+	    # making charts
+
         for i in range(TSNE.n_iter):
             plot = sns.scatterplot(TSNE.steps[i][:, 0],
                                    TSNE.steps[i][:, 1],
-                                   hue=y, palette=sns.color_palette('bright'), legend=None)
+                                   hue=y)
             plot.set_title(str(i + 1), fontsize=30)
             plot.axis('off')
             plot = plot.get_figure()
--- build/temp.win-amd64-3.6/Release/cmake_install.cmake
+++ build/temp.win-amd64-3.6/Release/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Admin/Desktop/Informatyka/Multicore-TSNE/multicore_tsne
+# Install script for directory: C:/Users/Admin/Desktop/Trace-TSNE/multicore_tsne
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Admin/Desktop/Informatyka/Multicore-TSNE/build/temp.win-amd64-3.6/Release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Admin/Desktop/Trace-TSNE/build/temp.win-amd64-3.6/Release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
